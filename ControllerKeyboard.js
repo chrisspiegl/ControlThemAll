6eb175003b1e7da0343bab8353195a803e04033a
Chris Spiegl <chris@chrisspiegl.com>
2022-01-11 18:59:02 +0100

    Mostly just formatting updates to xo standard.

--- ControllerKeyboard.js
+++ ControllerKeyboard.js
@@ -1,96 +1,98 @@
-import debug from 'debug'
-import { EventEmitter } from 'inf-ee'
-import { clipboard, keyboard, Key } from "@nut-tree/nut-js"
-import { asArray } from './utils.js'
+import debug from 'debug';
+import {EventEmitter} from 'inf-ee';
+import {clipboard, keyboard, Key} from '@nut-tree/nut-js';
+import {asArray} from './utils.js';
 
-const log = debug(`sio:keyboard`)
+const log = debug('sio:keyboard');
 
 export class ControllerKeyboard extends EventEmitter {
-  constructor(options = {}) {
-    super()
-    console.log(`Constructing Keyboard Controller`)
+	constructor(options = {}) {
+		super();
+		console.log('Constructing Keyboard Controller');
 
-    this.componentName = 'Keyboard Controller'
+		this._config = options.config || {};
+		this.componentName = 'Keyboard Controller';
+	}
 
-    return this
-  }
+	async connect(options = {}) {
+		this._config = options.config || {};
+		// Keyboard.nativeAdapter.keyboard.setKeyboardDelay(0)
+		// keyboard.config.autoDelayMs = 0
+		this.emit('connected');
+	}
 
-  async connect(options = {}) {
-    // keyboard.nativeAdapter.keyboard.setKeyboardDelay(0)
-    // keyboard.config.autoDelayMs = 0
-    this.emit('connected')
-  }
+	disconnect() {
+		this.emit('disconnect');
+	}
 
-  disconnect() {
-    this.emit('disconnect')
-  }
+	fire(keys) {
+		keys = asArray(keys).map(key => this.mapKey(key));
+		for (const key of keys) {
+			if (!key.keyCode || !Number.isInteger(key.keyCode, 10)) {
+				this.log('error', `Keyboard Combination Contains undefined or not a number: ${key.keyName} = ${key.keyCode}`);
+				return;
+			}
+		}
 
-  fire(keys) {
-    keys = asArray(keys).map((key) => this.mapKey(key))
-    for (const key of keys) {
-      if (!key.keyCode || !Number.isInteger(key.keyCode, 10)) {
-        this.log('error', `Keyboard Combination Contains undefined or not a number: ${key.keyName} = ${key.keyCode}`)
-        return
-      }
-    }
-    console.log(keys)
-    this.log('info', 'Sending Keyboard Shortcut:', keys.map((key) => key.keyName).join(' + '))
-    return keyboard.type.apply(keyboard, keys.map((key) => key.keyCode))
-  }
+		console.log(keys);
+		this.log('info', 'Sending Keyboard Shortcut:', keys.map(key => key.keyName).join(' + '));
+		return keyboard.type(...keys.map(key => key.keyCode));
+	}
 
-  pressKey(keys) {
-    keys = asArray(keys).map((key) => this.mapKey(key))
-    for (const key of keys) {
-      if (!key.keyCode || !Number.isInteger(key.keyCode, 10)) {
-        this.log('error', `Keyboard Combination Contains undefined or not a number: ${key.keyName} = ${key.keyCode}`)
-        return
-      }
-    }
-    this.log('info', `Pressing Keyboard Shortcut:`, keys.map((key) => key.keyName).join(' + '))
-    return keyboard.pressKey.apply(keyboard, keys.map((key) => key.keyCode))
+	pressKey(keys) {
+		keys = asArray(keys).map(key => this.mapKey(key));
+		for (const key of keys) {
+			if (!key.keyCode || !Number.isInteger(key.keyCode, 10)) {
+				this.log('error', `Keyboard Combination Contains undefined or not a number: ${key.keyName} = ${key.keyCode}`);
+				return;
+			}
+		}
 
-  }
+		this.log('info', 'Pressing Keyboard Shortcut:', keys.map(key => key.keyName).join(' + '));
+		return keyboard.pressKey(...keys.map(key => key.keyCode));
+	}
 
-  releaseKey(keys) {
-    keys = asArray(keys).map((key) => this.mapKey(key))
-    for (const key of keys) {
-      if (!key.keyCode || !Number.isInteger(key.keyCode, 10)) {
-        this.log('error', `Keyboard Combination Contains undefined or not a number: ${key.keyName} = ${key.keyCode}`)
-        return
-      }
-    }
-    this.log('info', `Releaseing Keyboard Shortcut:`, keys.map((key) => key.keyName).join(' + '))
-    return keyboard.releaseKey.apply(keyboard, keys.map((key) => key.keyCode))
-  }
+	releaseKey(keys) {
+		keys = asArray(keys).map(key => this.mapKey(key));
+		for (const key of keys) {
+			if (!key.keyCode || !Number.isInteger(key.keyCode, 10)) {
+				this.log('error', `Keyboard Combination Contains undefined or not a number: ${key.keyName} = ${key.keyCode}`);
+				return;
+			}
+		}
 
-  typeViaKeyboard(text) {
-    this.log('info', `Typing via Keyboard: ${text}`)
-    return keyboard.type(text)
-  }
+		this.log('info', 'Releaseing Keyboard Shortcut:', keys.map(key => key.keyName).join(' + '));
+		return keyboard.releaseKey(...keys.map(key => key.keyCode));
+	}
 
-  typeViaClipboard(text) {
-    clipboard.copy(text)
-    this.log('info', `Type via Clipboard: ${text}`)
-    return this.fire([Key.LeftSuper, Key.V])
-  }
+	typeViaKeyboard(text) {
+		this.log('info', `Typing via Keyboard: ${text}`);
+		return keyboard.type(text);
+	}
 
-  type(text) {
-    return this.typeViaClipboard(text)
-  }
+	typeViaClipboard(text) {
+		clipboard.copy(text);
+		this.log('info', `Type via Clipboard: ${text}`);
+		return this.fire([Key.LeftSuper, Key.V]);
+	}
 
-  mapKey(key) {
-    key = (key.length === 1) ? key.toUpperCase() : key
-    return {
-      key,
-      keyName: Number.isInteger(key) ? Key[`${key}`] : key ,
-      keyCode: Number.isInteger(key) ? key : Key[key]
-    }
-  }
+	type(text) {
+		return this.typeViaClipboard(text);
+	}
 
-  log(level, ...args) {
-    this.emit('log', { component: this.componentName, level: level.toLowerCase(), message: args })
-    log(`${level}: ${args.join(' ')}`)
-  }
+	mapKey(key) {
+		key = (key.length === 1) ? key.toUpperCase() : key;
+		return {
+			key,
+			keyName: Number.isInteger(key) ? Key[`${key}`] : key,
+			keyCode: Number.isInteger(key) ? key : Key[key],
+		};
+	}
+
+	log(level, ...args) {
+		this.emit('log', {component: this.componentName, level: level.toLowerCase(), message: args});
+		log(`${level}: ${args.join(' ')}`);
+	}
 }
 
-export default ControllerKeyboard
+export default ControllerKeyboard;
--- utils.js
+++ utils.js
@@ -1,8 +1,10 @@
-export const asArray = (x) => Array.isArray(x) ? x : [x]
+export const asArray = x => Array.isArray(x) ? x : [x];
 
-export const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2
+export function map(value, x1, y1, x2, y2) {
+	return (((value - x1) * (y2 - x2)) / (y1 - x1)) + x2;
+}
 
 export const getEnumByValue = (myEnum, enumValue) => {
-    let keys = Object.keys(myEnum).filter(x => myEnum[x] == enumValue)
-    return keys.length > 0 ? keys[0] : null
-}
+	const keys = Object.keys(myEnum).filter(x => myEnum[x] === enumValue);
+	return keys.length > 0 ? keys[0] : null;
+};
