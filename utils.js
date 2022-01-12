3d5b1cf102fb493c037dbe2ee8b9ae4373b661a4
Chris Spiegl <chris@chrisspiegl.com>
2022-01-12 10:35:13 +0100

    add diffByHash for objects in array comparison

--- utils.js
+++ utils.js
@@ -1,4 +1,7 @@
-export const asArray = x => Array.isArray(x) ? x : [x];
+import {differenceWith} from 'lodash-es';
+import objectHash from 'object-hash';
+
+export const asArray = x => (Array.isArray(x) ? x : [x]);
 
 export function map(value, x1, y1, x2, y2) {
 	return (((value - x1) * (y2 - x2)) / (y1 - x1)) + x2;
@@ -8,3 +11,5 @@ export const getEnumByValue = (myEnum, enumValue) => {
 	const keys = Object.keys(myEnum).filter(x => myEnum[x] === enumValue);
 	return keys.length > 0 ? keys[0] : null;
 };
+
+export const diffByHash = (a, b) => differenceWith(a, b, (a, b) => objectHash(a) === objectHash(b));
